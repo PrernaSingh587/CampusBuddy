1fbaa4017e9fba9a50d51710243d338be5dbf05f
Prerna Singh <prernasingh@Prernas-MacBook-Air.local>
2022-09-01 21:23:08 +0530

    adding mongo

--- src/main/java/com/example/campusbuddy/repository/CollegeRepository.java
+++ src/main/java/com/example/campusbuddy/repository/CollegeRepository.java
@@ -2,10 +2,11 @@ package com.example.campusbuddy.repository;
 
 import com.example.campusbuddy.entity.College;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CollegeRepository extends JpaRepository<College, Long> {
+public interface CollegeRepository extends MongoRepository<College, Long> {
     Boolean existsCollegeByCollege(String college);
 
     College findByCollege(String college);
--- src/main/java/com/example/campusbuddy/repository/UserRepository.java
+++ src/main/java/com/example/campusbuddy/repository/UserRepository.java
@@ -2,18 +2,25 @@ package com.example.campusbuddy.repository;
 
 import com.example.campusbuddy.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, Long> {
 
+    @Query("{ 'username' : ?0 }")
     List<User> findUserByUsername(String username);
+    @Query("{ 'email' : ?0 }")
     List<User>findUserByEmail(String email);
+    @Query("{ 'name' : ?0 }")
     List<User>findUserByName(String name);
+    @Query("{ 'college.name' : ?0 }")
     List<User>findUserByCollege(String College);
+
     Boolean existsUserByUsername(String username);
     Boolean existsUserByEmail(String email);
 
